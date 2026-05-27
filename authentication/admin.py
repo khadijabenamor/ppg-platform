c686e4e60b70842904afad905168a407a1e30b93
osarra720-design <osarra720@gmail.com>
2026-05-28 00:10:11 +0200

    affichage admin : sarra

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,14 +2,55 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Abonnement
 
-
+'''
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ("username", "email", "role", "is_premium", "created_at")
     list_filter  = ("role",)
     fieldsets    = UserAdmin.fieldsets + (
         ("Infos PPG", {"fields": ("role", "avatar")}),
+    )'''
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "get_abonnement",
+        "superviseur",
+        "created_at",
     )
+
+    list_filter = (
+        "role",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Infos PPG",
+            {
+                "fields": (
+                    "role",
+                    "avatar",
+                    "superviseur",
+                )
+            },
+        ),
+    )
+
+    def get_abonnement(self, obj):
+        try:
+            return obj.abonnement.type
+        except:
+            return "Aucun"
+
+    get_abonnement.short_description = "Abonnement"
 
 
 @admin.register(Abonnement)
